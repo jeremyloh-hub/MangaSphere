ff4b9533ef596d56bf7150c6db3031a318a73cda
jeremyloh-hub <eziorain@gmail.com>
2023-03-15 21:25:47 +0800

    add isAuth in route

--- routes/mangas.js
+++ routes/mangas.js
@@ -2,12 +2,33 @@ var express = require("express");
 var router = express.Router();
 const mangaCtrl = require("../controllers/mangas");
 
-/* GET home page. */
+const isAuth = async (req, res, next) => {
+  if (req.session.userid) {
+    const user = await User.findById(req.session.userid).exec();
+    res.locals.user = user;
+    next();
+  } else {
+    res.status(403).send(req.session);
+  }
+};
+
 router.get("/:id", mangaCtrl.showManga);
-router.get("/member/:id", mangaCtrl.showMangaMem);
-router.post("/member/:id", mangaCtrl.addReview);
-router.get("/member/:mangaID/:reviewID/:userID/edit", mangaCtrl.showEdit);
-router.put("/member/:mangaID/:reviewID/:userID/edit", mangaCtrl.editReview);
-router.delete("/member/:mangaID/:reviewID/:userID", mangaCtrl.deleteReview);
+router.get("/member/:id", isAuth, mangaCtrl.showMangaMem);
+router.post("/member/:id", isAuth, mangaCtrl.addReview);
+router.get(
+  "/member/:mangaID/:reviewID/:userID/edit",
+  isAuth,
+  mangaCtrl.showEdit
+);
+router.put(
+  "/member/:mangaID/:reviewID/:userID/edit",
+  isAuth,
+  mangaCtrl.editReview
+);
+router.delete(
+  "/member/:mangaID/:reviewID/:userID",
+  isAuth,
+  mangaCtrl.deleteReview
+);
 
 module.exports = router;
